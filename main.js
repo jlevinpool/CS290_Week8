3499a287fd7cd530b6372a925eca7998c82f910a
jlevinpool <poolj@onid.oregonstate.edu>
2016-02-26 14:21:01 -0800

    Initial File Commit

---
+++ main.js
@@ -0,0 +1,41 @@
+/* **************************************************************************
+** Author: James Pool
+** ONID: 932664412
+** OSU Email: poolj@oregonstate.edu
+** Date: 18 February 2016
+**
+** Program Filename: main.js
+** Description: Week 8 Activity - HTTP Requests
+*************************************************************************** */
+
+/* Express and Middleware */
+var express = require('express');
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var bodyParser = require('body-parser');
+var credentials = require('./credentials.js');
+var request = require('request');
+
+/* Handlebars Setup */
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+/* Body Parser Setup */
+app.use(bodyParser.urlencoded({extended:false});
+app.use(bodyParser.json());
+
+/* CSS Setup */
+app.use(express.static(__dirname + '/public'));
+
+/* Application Port */
+app.set('port', 3081);
+
+/* Home Catcher */
+app.get('/' function(req,res){
+	res.render('home');
+}
+
+/* Start Application */
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; press ctrl-c to terminate.');
+});
