e044aa0b1c3fead98376166ee0b92ec01e3c3d80
jlevinpool <poolj@onid.oregonstate.edu>
2016-02-26 16:11:55 -0800

    Added 'Start Over' Option

--- main.js
+++ main.js
@@ -40,21 +40,69 @@ app.set('port', 3081);
 
 /* GET Catcher */
 app.get('/',function(req,res,next){
+	console.log("GET");
+	var context = {};
+	//If there is no session, go to the main page.
+	if(!req.session.name){
+		res.render('newSession', context);
+		return;
+	}
+	console.log(req.body);
+	context.name = req.session.name;
+	context.toDoCount = req.session.toDo.length || 0;
+	context.toDo = req.session.toDo || [];
+	res.render('toDo',context);
+});
+
+/* POST Catcher */
+app.post('/',function(req,res,next){
   var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
   //If there is no session, go to the main page.
   if(!req.session.name){
     res.render('newSession', context);
     return;
   }
+  
+	if(req.body['resetForm']) {
+		res.render('newSession', context);
+		return;
+	}
+
+  if(req.body['Add Item']){
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId});
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
   context.name = req.session.name;
-  context.toDoCount = req.session.toDo.length || 0;
-  context.toDo = req.session.toDo || [];
-  console.log(context.toDo);
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
   res.render('toDo',context);
 });
 
-/* POST Catcher */
+/* ERROR Handler */
+app.use(function(req,res){
+	res.status(404);
+	res.render('404_-_Not_Found');
+});
 
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500_-_Internal_Server_Error');
+});
 
 /* Start Application */
 app.listen(app.get('port'), function(){
